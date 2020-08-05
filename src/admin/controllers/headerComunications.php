2e448f2d3d4eafcd2739de3b10a9f2941fcb3fb2
Douglas Gonçalves <douglas.goncalves@grupofolha.com.br>
2020-08-04 21:08:41 -0300

    Comunicações internamente ao módulo

---
+++ src/admin/controllers/headerComunications.php
@@ -0,0 +1,58 @@
+<?php
+
+    namespace comunication\admin\controllers;
+
+    use driver\control\action;
+    use comunication\common\models\comunications;
+
+    class headerComunications extends action
+    {
+        const _LOCAL = __DIR__;
+
+        /**
+         * Função a ser executada no contexto da action
+         *
+         * @param array $info
+         * @return void
+         */
+        public function main(array $info)
+        {
+            $layout = __DIR__.'../responses/headerComunications.phtml';
+            self::setLayout($layout);
+
+            if(empty(new comunications())){
+                return $this->view(array('isModule' => false));
+            }
+
+            // comments
+            $this->param('comments', (new comunications())->comunicationByQuality(1, 1));
+            // e-mails
+            $this->param('emails', (new comunications())->comunicationByQuality(2, 1));
+            // notifications
+            $this->param('notifications',(new comunications())->comunicationByQuality(3, 1));
+
+            return $this->view(array('isModule' => true));
+        }
+
+        /**
+         * Para ser disparado antes
+         *
+         * @return void
+         */
+        public function _before()
+        {
+
+        }
+
+        /**
+         * Para ser disparado depois
+         *
+         * @return void
+         */
+        public function _after()
+        {
+
+        }
+    }
+
+?>
